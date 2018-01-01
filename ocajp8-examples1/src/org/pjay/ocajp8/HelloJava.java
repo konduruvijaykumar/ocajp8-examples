c49d5677699d7214b490c5564272904fa24d2c2e
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-01 19:21:30 +0530

    Vijay - 2018/01/01 - Testing commits via intellij

---
+++ ocajp8-examples1/src/org/pjay/ocajp8/HelloJava.java
@@ -0,0 +1,10 @@
+package org.pjay.ocajp8;
+
+public class HelloJava {
+
+    public static void main(String... args) {
+        System.out.println("Hello Java..");
+        System.out.println("args length is :: " + args.length);
+    }
+
+}
