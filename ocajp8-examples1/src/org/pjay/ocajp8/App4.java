2b36edeb3eb34f43c7eaf308edcbcfdac8e5f20b
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-09 08:57:44 +0530

    Vijay - 2018/01/09 - ocajp8 examples

---
+++ ocajp8-examples1/src/org/pjay/ocajp8/App4.java
@@ -0,0 +1,9 @@
+package org.pjay.ocajp8;
+
+public class App4 {
+
+    public static final void main(String[] args) {
+        System.out.println("We have added final to main method, but still works");
+    }
+
+}
