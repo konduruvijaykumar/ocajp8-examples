ad9c0dcfcb69ccfed276125c896e1775a7751337
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-10 09:42:48 +0530

    Vijay - 2018/01/10 - ocajp8 examples

---
+++ ocajp8-examples2/src/org/pjay/ocajp8/App2.java
@@ -0,0 +1,14 @@
+package org.pjay.ocajp8;
+
+public class App2 {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        System.out.println(counter);
+        System.out.println(++counter);
+        System.out.println(counter);
+        System.out.println(counter--);
+        System.out.println(counter);
+    }
+
+}
