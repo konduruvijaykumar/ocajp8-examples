ed2da3aca7cd3b8e578853576b7a7619a117e7ca
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-17 01:56:08 +0530

    Vijay - 2018/01/17 - ocajp8 examples

---
+++ ocajp8-examples3/src/org/pjay/ocajp8/App4.java
@@ -0,0 +1,19 @@
+package org.pjay.ocajp8;
+
+public class App4 {
+
+    public static void main(String[] args) {
+        int arr[][] = new int[2][];
+        // Results in NullPointerException as size of major or row is only declared, in the below line when assigning value
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+
+        System.out.println("arr[1][1] >>> " + arr[1][1]);
+    }
+
+}
