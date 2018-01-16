2a9c94749b207043e0c682a524d37bf4f7890df1
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-16 15:33:08 +0530

    Vijay - 2018/01/16 - ocajp8 examples

---
+++ ocajp8-examples1/src/org/pjay/ocajp8/App6.java
@@ -0,0 +1,29 @@
+package org.pjay.ocajp8;
+
+public class App6 {
+
+    public static void main(String[] args) {
+
+        int int1 = 1+2*3;
+        int int2 = (1+2)*3;
+        System.out.println("int1 " + int1);
+        System.out.println("int2 " + int2);
+
+        boolean boolean1 = true || true && false;
+        boolean boolean2 = (true || true) && false;
+        System.out.println("boolean1 " + boolean1);
+        System.out.println("boolean2 " + boolean2);
+
+        int x = 4;
+        int y = 4;
+        // Note: Important Please verify
+        // pre and post unary operators are taking same precedence, hence it is evaluated below with left to right instead post being first
+        int result1 = x++ + --x + ++x + x--;
+        int result2 = y-- + ++y + --y + y++;
+        System.out.println("x " + x);
+        System.out.println("result1 " + result1);
+        System.out.println("y " + y);
+        System.out.println("result2 " + result2);
+    }
+
+}
