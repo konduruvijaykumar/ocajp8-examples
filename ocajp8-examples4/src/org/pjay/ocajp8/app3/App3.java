88dc0449267a7947c66f0343430a321e200767f7
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-18 19:09:35 +0530

    Vijay - 2018/01/18 - ocajp8 examples

--- ocajp8-examples4/src/org/pjay/ocajp8/app3/App3.java
+++ ocajp8-examples4/src/org/pjay/ocajp8/app3/App3.java
@@ -10,12 +10,19 @@ public class App3 extends App2 {
     }
 
     public  void print(int y){
+        // https://stackoverflow.com/questions/20404977/accessing-a-protected-variable-from-a-subclass-outside-package
+        // https://stackoverflow.com/questions/22139513/protected-variables-can-be-accessed-within-child-class-or-child-object
         System.out.println("y " + y);
         App2 app2 = new App2();
+        App3 app3 = new App3();
         // Compiler Error: 'x' has protected access in 'org.pjay.ocajp8.App2'
         //System.out.println(app2.x);
         // This is a inherited property so can be accessed directly
         System.out.println("x " + x);
+        System.out.println("x " + app3.x);
+        System.out.println("x " + this.x);
+        System.out.println("x " + super.x);
+        //System.out.println(app2.x);//Not allowed
     }
 
 }
