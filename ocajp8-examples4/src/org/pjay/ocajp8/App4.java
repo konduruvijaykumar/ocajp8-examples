0ace0d012ca0bade158f9b6add6eeab704253734
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-18 19:28:44 +0530

    Vijay - 2018/01/18 - ocajp8 examples

---
+++ ocajp8-examples4/src/org/pjay/ocajp8/App4.java
@@ -0,0 +1,11 @@
+package org.pjay.ocajp8;
+
+public class App4 {
+
+    public static void main(String[] args) {
+        App42 app42 = new App42();
+        app42.method1(5);
+        app42.method1(5L);
+    }
+
+}
