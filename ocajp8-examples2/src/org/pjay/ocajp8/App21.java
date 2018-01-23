48c2cad536c59567ec61e5ccb2d97a5a65424308
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-23 18:42:48 +0530

    Vijay - 2018/01/23 - ocajp8 examples

--- ocajp8-examples2/src/org/pjay/ocajp8/App21.java
+++ ocajp8-examples2/src/org/pjay/ocajp8/App21.java
@@ -31,6 +31,18 @@ public class App21 {
                 System.out.println("y " + y);
             }*/
         }
+
+        java.util.List<String> names = new java.util.ArrayList<String>();
+        names.add("Lisa");
+        names.add("Kevin");
+        names.add("Roger");
+        for(int i=0; i<names.size(); i++) {
+            String name = names.get(i);
+            if(i>0) {
+                System.out.print(", ");
+            }
+            System.out.print(name);
+        }
     }
 
 }
