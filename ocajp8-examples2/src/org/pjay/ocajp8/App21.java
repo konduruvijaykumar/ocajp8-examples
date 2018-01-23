6757bc59205f0ca92affd90f67a35c879cfa0b83
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-23 17:24:11 +0530

    Vijay - 2018/01/23 - ocajp8 examples

---
+++ ocajp8-examples2/src/org/pjay/ocajp8/App21.java
@@ -0,0 +1,34 @@
+package org.pjay.ocajp8;
+
+public class App21 {
+
+    public static void main(String[] args) {
+        // Block 1
+        {
+            int x = 0;
+            for(long y = 0, z = 4;x < 5 && y <10;x++, y++){
+                System.out.println("y " + y);
+            }
+            System.out.println("x " + x);
+        }
+
+        // Block 2
+        {
+            int x = 0;
+            // Compiler error - Variable 'x' is already defined in the scope
+/*            for(long y = 0, x = 4;x < 5 && y <10;x++, y++){//for(int y = 0, x = 4;x < 5 && y <10;x++, y++)for(long y = 0, x = 4;x < 5 && y <10;x++, y++){
+                System.out.println("y " + y);
+            }*/
+            System.out.println("x " + x);
+        }
+
+        //Block 3
+        {
+            // Compiler error - ';' expected. Identifier expected. -- As variables y and x are diff types declared in same line or statement without end or termination
+/*            for(long y = 0,int x = 4;x < 5 && y <10;x++, y++){
+                System.out.println("y " + y);
+            }*/
+        }
+    }
+
+}
