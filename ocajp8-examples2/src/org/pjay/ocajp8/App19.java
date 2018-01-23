c44b751db406bb4e5661070aefb3d0ba3db9e2a1
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-23 15:59:55 +0530

    Vijay - 2018/01/23 - ocajp8 examples

---
+++ ocajp8-examples2/src/org/pjay/ocajp8/App19.java
@@ -0,0 +1,13 @@
+package org.pjay.ocajp8;
+
+public class App19 {
+
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            x++;
+        }while (false);
+        System.out.println("x " + x);
+    }
+
+}
