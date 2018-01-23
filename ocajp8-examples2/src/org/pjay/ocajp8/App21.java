25dd3cf1c3f17ffe088f96a34bfa0e6c41754060
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-23 17:36:44 +0530

    Vijay - 2018/01/23 - ocajp8 examples

--- ocajp8-examples2/src/org/pjay/ocajp8/App21.java
+++ ocajp8-examples2/src/org/pjay/ocajp8/App21.java
@@ -24,6 +24,8 @@ public class App21 {
 
         //Block 3
         {
+            // Compiler error -- Identifier expected
+            //int i = 0,long j = 0;
             // Compiler error - ';' expected. Identifier expected. -- As variables y and x are diff types declared in same line or statement without end or termination
 /*            for(long y = 0,int x = 4;x < 5 && y <10;x++, y++){
                 System.out.println("y " + y);
