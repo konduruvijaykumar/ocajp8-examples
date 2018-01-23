f355cfa39fc894313de3586765a58a9ca802e085
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-23 16:29:18 +0530

    Vijay - 2018/01/23 - ocajp8 examples

---
+++ ocajp8-examples2/src/org/pjay/ocajp8/App20.java
@@ -0,0 +1,26 @@
+package org.pjay.ocajp8;
+
+public class App20 {
+
+    public static void main(String[] args) {
+/*        for (int i = 0;i<10;i++){
+            System.out.println("i " + i);
+        }*/
+
+        for (int i = initBlock();i<10;i = updateBlock(i)){
+            System.out.println("i " + i);
+        }
+
+    }
+
+    private static int initBlock(){
+        System.out.println("Init Block");
+        return 0;
+    }
+
+    private static int updateBlock(int i){
+        System.out.println("Update Block");
+        //return i++;
+        return i+1;
+    }
+}
