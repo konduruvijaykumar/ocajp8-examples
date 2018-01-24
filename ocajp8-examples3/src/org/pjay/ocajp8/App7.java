aac6b3c15900093a20dddb14200499971d075ac5
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-24 13:49:50 +0530

    Vijay - 2018/01/24 - ocajp8 examples

---
+++ ocajp8-examples3/src/org/pjay/ocajp8/App7.java
@@ -0,0 +1,14 @@
+package org.pjay.ocajp8;
+
+public class App7 {
+
+    public static void main(String[] args) {
+        // The expression is evaluated left to right
+        System.out.println(1 + 2); //3
+        System.out.println("a" + "b"); //ab
+        System.out.println("a" + "b" + 3); //ab3
+        System.out.println(1+2+"c"); //3c
+        System.out.println("a"+2+5); //a25
+    }
+
+}
