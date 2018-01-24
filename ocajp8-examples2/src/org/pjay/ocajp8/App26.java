d91d4bafc3e77a4624a523327bd3c091b14d364f
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-24 13:51:48 +0530

    Vijay - 2018/01/24 - ocajp8 examples

--- ocajp8-examples2/src/org/pjay/ocajp8/App26.java
+++ ocajp8-examples2/src/org/pjay/ocajp8/App26.java
@@ -20,6 +20,21 @@ public class App26 {
                 i++;
                 System.out.println("Hello World " + i);
             }
+
+            int m = 9, n = 1, x = 0;
+            x += m + n;
+            System.out.println("x " + x);
+        }
+
+        {
+            int count = 0;
+            ROW_LOOP: for(int row = 1; row <=3; row++)
+                for(int col = 1; col <=2 ; col++) {
+                    if(row * col % 2 == 0) continue ROW_LOOP;
+                    count++;
+                }
+            System.out.println(count);
+            System.out.println(count);
         }
 
     }
