8207e809bc7484df67a686f08e0c1ac7cd1ee758
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-24 14:48:11 +0530

    Vijay - 2018/01/24 - ocajp8 examples

--- ocajp8-examples3/src/org/pjay/ocajp8/App7.java
+++ ocajp8-examples3/src/org/pjay/ocajp8/App7.java
@@ -9,6 +9,15 @@ public class App7 {
         System.out.println("a" + "b" + 3); //ab3
         System.out.println(1+2+"c"); //3c
         System.out.println("a"+2+5); //a25
+
+        int three = 3;
+        String four = "4";
+        System.out.println(1 + 2 + three + four);
+
+        String s = "1";
+        s += "2";
+        s += 3;
+        System.out.println("s " + s);
     }
 
 }
