40ab0e44c98599f63bb8a2b151bfad56c41d0601
Vijay Konduru <konduruvijaykumar@gmail.com>
2018-01-25 18:48:32 +0530

    Vijay - 2018/01/25 - ocajp8 examples

--- ocajp8-examples3/src/org/pjay/ocajp8/App12.java
+++ ocajp8-examples3/src/org/pjay/ocajp8/App12.java
@@ -17,6 +17,11 @@ public class App12 {
         System.out.println(Arrays.toString(types));
         System.out.println(Arrays.toString(ids1));
         System.out.println(types1);
+
+        int[][] vars1; // 2D array
+        int vars2 [][]; // 2D array
+        int[] vars3[]; // 2D array
+        int[] vars4 [], space [][]; // a 2D AND a 3D array -- (spaces)
     }
 
 }
